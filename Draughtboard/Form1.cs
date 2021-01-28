93b90cfcba6a385d138e5c9100b1cff6060a24c1
iremcaliskan <iremcaliskan@eskisehir.edu.tr>
2021-01-29 02:07:41 +0300

    Add project files.

---
+++ Draughtboard/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draughtboard
+{
+    public partial class Form1 : Form //Inheritance
+    {
+        public Form1()
+        {
+            InitializeComponent(); //Constructor
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateButtons();
+
+        }
+
+        private void GenerateButtons()
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i <= buttons.GetUpperBound(0); i++)
+            {
+
+                for (int j = 0; j <= buttons.GetUpperBound(1); j++) //GetUpperbound0 = 7
+                {
+                    buttons[i, j] = new Button(); // Button bir classtır, örneğini oluşturmuş oluruz.
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    left += 50;
+                    buttons[i, j].Top = top
+                        ;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+
+                    }
+                    this.Controls.Add(buttons[i, j]); // Button'u ekrana koyar.
+                }
+                left = 0; // Diğer satıra geçildiğinde sola geçmesi için sıfırlanması gerekiyor.
+                top += 50; // Diğer satıra geçildiğinde aşağıya geçilmesi için arttırılması gerekiyor.x
+            }
+        }
+    }
+}
